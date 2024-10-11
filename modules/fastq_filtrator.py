86ed1e8037a2e062ed0b15ae89db1b371d512080
opalinn <141135369+opalinn@users.noreply.github.com>
2024-10-11 18:24:00 +0300

    Update functions in fastq_filtrator.py

--- modules/fastq_filtrator.py
+++ modules/fastq_filtrator.py
@@ -1,6 +1,7 @@
 def filter_by_quality(
-    seqs: dict, seqs_output: dict, quality_threshold: int = 0
+    seqs: dict, quality_threshold: int = 0
 ) -> dict:
+    seqs_output = {}
     for key, value in seqs.items():
         sum_of_scores = 0
         quality_string = value[1]
@@ -12,17 +13,19 @@ def filter_by_quality(
     return seqs_output
 
 
-def filter_by_length(seqs: dict, length_bounds: tuple, seqs_output: dict) -> dict:
-    for key, value in seqs.items():
+def filter_by_length(seqs_output: dict, length_bounds: tuple) -> dict:
+    filtered_by_length = {}
+    for key, value in seqs_output.items():
         if length_bounds[0] <= len(value[0]) <= length_bounds[1]:
-            seqs_output[key] = value
-    return seqs_output
+            filtered_by_length[key] = value
+    return filtered_by_length
 
 
-def filter_by_gc_content(seqs: dict, gc_bounds: tuple, seqs_output: dict) -> dict:
-    for key, value in seqs.items():
+def filter_by_gc_content(filtered_by_length: dict, gc_bounds: tuple) -> dict:
+    filtered_by_gc = {}
+    for key, value in filtered_by_length.items():
         gc_sum = value[0].count("G") + value[0].count("C")
         gc_content = (gc_sum / len(value[0])) * 100
         if gc_bounds[0] <= gc_content <= gc_bounds[1]:
-            seqs_output[key] = value
-    return seqs_output
+            filtered_by_gc[key] = value
+    return filtered_by_gc
